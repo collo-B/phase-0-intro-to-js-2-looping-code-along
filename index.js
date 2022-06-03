db7885fdf3f94f11e664ac85740c1452a73325ef
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-06-03 08:36:38 +0300

    commit tree f2912d212f79152135c8c3f27ee27bf63fc8b9ad

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let names = [];
+function writeCards(names, event) {
+const message = [];
+for (let i = 0; i < names.length; i++) {
+    const text = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    message.push(text);
+}
+return message;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
